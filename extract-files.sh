ee62dc12ba6c69948254620858d365f37978678d
bengris32 <bengris32@protonmail.ch>
2024-08-21 20:45:15 +0100

    spaced: Link camera dependencies against libsensorndkbridge-hidl
    Change-Id: Ifdd4f84dd86839abc5cf17bbc37c3cb268fc3a95
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>

--- extract-files.sh
+++ extract-files.sh
@@ -107,7 +107,9 @@ function blob_fixup {
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
         vendor/bin/mnld|\
-        vendor/lib64/libaalservice.so)
+        vendor/lib64/libaalservice.so|\
+        vendor/lib64/libcam.utils.sensorprovider.so|\
+        vendor/lib64/liboplus_mtkcam_lightsensorprovider.so)
             "${PATCHELF}" --replace-needed "libsensorndkbridge.so" "libsensorndkbridge-hidl.so" "${2}"
             ;;
     esac
