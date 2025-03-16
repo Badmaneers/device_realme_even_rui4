9ee663589669672f4bced03a7041dd5cfd308eae
Michael Bestas <mkbestas@lineageos.org>
2025-03-16 12:52:36 +0530

    spaced: Shim few libs with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,10 @@ function blob_fixup {
         vendor/lib/hw/audio.primary.mt6781.so)
              "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
              ;;
+        odm/bin/hw/vendor.oplus.hardware.charger@1.0-service|vendor/lib*/libnvram.so|vendor/lib*/libsysenv.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
+            ;;
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
             grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
             ;;
