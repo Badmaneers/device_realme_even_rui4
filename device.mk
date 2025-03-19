ae011aacd60b8546cd6a7e7cb6ef27e47b93b1b8
Badmaneers <dukebraham24@gmail.com>
2025-03-19 09:57:05 +0000

    Add USB vendor packages to PRODUCT_PACKAGES in device.mk

--- device.mk
+++ device.mk
@@ -162,6 +162,26 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MtkInCallService
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0.vendor \
+    android.hardware.usb@1.1.vendor \
+    android.hardware.usb@1.2.vendor \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.0.vendor \
+    android.hardware.usb.gadget@1.1.vendor
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     wpa_supplicant \
