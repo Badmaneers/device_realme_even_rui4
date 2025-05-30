7f343c2e875e4390d62e5af4a0abcb372eb771d1
HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>
2025-05-30 19:38:46 +0000

    spaced: libui_shim: Use libui_shim from hardware/lineage/compat

--- device.mk
+++ device.mk
@@ -498,7 +498,7 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
 
 PRODUCT_PACKAGES += \
-    libshim_ui
+    libui_shim
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
