4c344f4405d6edfda7a950a3248d98af50d5a9d9
Badmaneers <dukebraham24@gmail.com>
2025-06-16 06:11:24 +0530

    Update git clone commands in vendorsetup.sh for consistency and accuracy

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,17 @@
 echo start cloning repos
 VT=vendor/realme/even/even-vendor.mk
-if ! [ -a $VT ]; then git clone https://github.com/Badmaneers/vendor_realme_even_rui4 vendor/realme/even
+if ! [ -a $VT ]; then git clone https://github.com/Badmaneers/vendor_realme_even_rui4 -bb main-oss vendor/realme/even
 fi
 KT=kernel/realme/even/build.sh
-if ! [ -a $KT ]; then git clone --recurse-submodules --depth=1 https://github.com/Badmaneers/kernel_even_k4.19 kernel/realme/even
+if ! [ -a $KT ]; then git clone --recurse-submodules --depth=1 https://github.com/Badmaneers/kernel_even_4.19 kernel/realme/even
 fi
 MTK_SEPOLICY=device/mediatek/sepolicy_vndr/SEPolicy.mk
-if ! [ -a $MTK_SEPOLICY ]; then git clone https://github.com/realme-mt6781-dev/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+if ! [ -a $MTK_SEPOLICY ]; then git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
 fi
 MTK=hardware/mediatek/Android.bp
-if ! [ -a $MTK ]; then git clone https://github.com/realme-mt6781-dev/android_hardware_mediatek hardware/mediatek
+if ! [ -a $MTK ]; then git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-21  hardware/mediatek
 fi
 OPLUS=hardware/oplus/Android.bp
-if ! [ -a $OPLUS ]; then git clone https://github.com/LineageOS/android_hardware_oplus hardware/oplus
+if ! [ -a $OPLUS ]; then git clone https://github.com/LineageOS/android_hardware_oplus -b lineage-21 hardware/oplus
 fi
 echo end cloning
