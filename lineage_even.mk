ecca2e613d336ac54f0997d67b92ac8725b8d231
Badmaneers <dukebraham24@gmail.com>
2026-04-16 08:35:32 +0530

    even: Initial device tree for Realme even (MT6768)

---
+++ lineage_even.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/realme/even/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_even
+PRODUCT_DEVICE := even
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3430
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+# Build info
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	BuildFingerprint=realme/RMX3430/RED8AF:13/SP1A.210812.016/R.182c3f7_cf1b8:user/release-keys \
+    DeviceName=RMX3430 \
+    DeviceProduct=RMX3430 \
+    SystemDevice=RMX3430 \
+    SystemName=RMX3430
