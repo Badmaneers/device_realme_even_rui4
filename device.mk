2ad9f2bb7987c1bdbc1a997ddb1c3341a3c6bde4
Badmaneers <dukebraham24@gmail.com>
2026-04-23 20:54:51 +0530

    even: Remove obsolete USB vendor packages from PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -413,11 +413,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0.vendor \
-    android.hardware.usb@1.1.vendor \
-    android.hardware.usb@1.2.vendor \
     android.hardware.usb@1.3.vendor \
-    android.hardware.usb.gadget@1.0.vendor \
     android.hardware.usb.gadget@1.1.vendor
 
 # Vendor service manager
