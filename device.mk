a9cd89eb083b37198f96836dac48f45208c319b8
Badmaneers <dukebraham24@gmail.com>
2026-04-25 12:06:39 +0530

    even: Add ScopedWakelock support and enable vibrator service for MediaTek

--- device.mk
+++ device.mk
@@ -373,6 +373,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_sensors \
     libsensorndkbridge \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.frameworks.sensorservice@1.0.vendor \
@@ -412,6 +413,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.1.vendor
+
+# Vibrator
+TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.mediatek
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
