0558e447ad057586c6f1058076da55d5a653015e
Badmaneers <dukebraham24@gmail.com>
2026-06-24 15:59:09 +0530

    even: Add libinit_even soong config and remove deprecated keymaster V3 NDK

--- device.mk
+++ device.mk
@@ -166,13 +166,15 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libhidlmemory.vendor
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_even)
+
 # Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Keymaster
 PRODUCT_PACKAGES += \
-   android.hardware.keymaster-V3-ndk_platform.vendor \
    android.hardware.keymaster@3.0.vendor \
    android.hardware.keymaster@4.0.vendor \
    android.hardware.keymaster@4.1.vendor \
