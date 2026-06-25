60f2fb3adfd88d8fe812985efea05bf88d26036a
Badmaneers <dukebraham24@gmail.com>
2026-06-25 07:52:56 +0530

    even: Add Oplus NDK AIDL backends, sensors 1.0-convert-shared, and custom USB gadget

--- device.mk
+++ device.mk
@@ -175,9 +175,9 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-imag
 
 # Keymaster
 PRODUCT_PACKAGES += \
-   android.hardware.keymaster@3.0.vendor \
-   android.hardware.keymaster@4.0.vendor \
-   android.hardware.keymaster@4.1.vendor \
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.1.vendor \
    libkeymaster4.vendor:64 \
    libkeymaster4support.vendor:64 \
    libkeymaster4_1support.vendor:64 \
@@ -191,6 +191,11 @@ PRODUCT_PACKAGES += \
    libnetutils.vendor
 
 $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
+
+# Oplus
+PRODUCT_PACKAGES += \
+    vendor.oplus.hardware.osense.client-V1-ndk_platform \
+    vendor.oplus.hardware.performance-V1-ndk_platform
 
 # Lights
 PRODUCT_PACKAGES += \
@@ -357,6 +362,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@1.0-convert-shared.vendor \
 
 PRODUCT_PACKAGES += \
     libdumpstateutil.vendor:64
@@ -372,7 +378,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/oplus \
-    hardware/lineage/compat \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/mediatek/libmtkperf_client \
@@ -390,6 +395,9 @@ PRODUCT_PACKAGES += \
     vendor.lineage.touch-service.oplus
 
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
+
+# Use device-specific init.mt6768.usb.rc instead of hardware/mediatek generic
+$(call soong_config_set,mediatek_gadget,use_custom_usb_gadget_rc,true)
 
 # USB
 PRODUCT_PACKAGES += \
