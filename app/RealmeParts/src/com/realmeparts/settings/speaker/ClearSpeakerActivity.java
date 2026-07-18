b20dc991fa791a2db77a86597158d8d397ad43ed
Badmaneers <dukebraham24@gmail.com>
2026-07-18 05:30:00 +0530

    even: realmeparts: Refactor ClearSpeaker to use Fragment-based layout

--- app/RealmeParts/src/com/realmeparts/settings/speaker/ClearSpeakerActivity.java
+++ app/RealmeParts/src/com/realmeparts/settings/speaker/ClearSpeakerActivity.java
@@ -1,104 +1,42 @@
+/*
+ * Copyright (C) 2020 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.realmeparts.settings.speaker;
 
-import android.app.Activity;
-import android.content.Context;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.MenuItem;
 
-import com.realmeparts.settings.R;
+import androidx.fragment.app.Fragment;
 
-public class ClearSpeakerActivity extends Activity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
-    private Button mButton;
-    private TextView mStatus;
-    private AudioTrack mAudioTrack;
-    private boolean mPlaying = false;
-    private Handler mHandler = new Handler();
-
-    private static final int DURATION_SECONDS = 60;
-    private static final int SAMPLE_RATE = 44100;
+public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.clear_speaker_settings);
 
-        mButton = findViewById(R.id.clear_speaker_button);
-        mStatus = findViewById(R.id.clear_speaker_status);
-
-        mButton.setOnClickListener(v -> {
-            if (mPlaying) {
-                stop();
-            } else {
-                start();
-            }
-        });
-    }
-
-    private void start() {
-        AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
-        int maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-
-        if (currentVolume < maxVolume) {
-            mStatus.setText("Please set media volume to maximum");
-            return;
+        Fragment fragment = getSupportFragmentManager().findFragmentById(com.android.settingslib.collapsingtoolbar.R.id.content_frame);
+        ClearSpeakerFragment clearSpeakerFragment;
+        if (fragment == null) {
+            clearSpeakerFragment = new ClearSpeakerFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(com.android.settingslib.collapsingtoolbar.R.id.content_frame, clearSpeakerFragment)
+                    .commit();
         }
-
-        int bufferSize = AudioTrack.getMinBufferSize(SAMPLE_RATE,
-                AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
-
-        mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE,
-                AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
-                bufferSize, AudioTrack.MODE_STREAM);
-
-        // Generate a frequency sweep from 200Hz to 20kHz
-        final short[] buffer = new short[bufferSize];
-        final double duration = DURATION_SECONDS;
-        final double startFreq = 200;
-        final double endFreq = 20000;
-        final long totalSamples = (long) (SAMPLE_RATE * duration);
-
-        mAudioTrack.play();
-        mPlaying = true;
-        mButton.setText("Stop");
-        mStatus.setText("Playing...");
-
-        new Thread(() -> {
-            for (long i = 0; i < totalSamples && mPlaying; i += buffer.length) {
-                for (int j = 0; j < buffer.length && (i + j) < totalSamples; j++) {
-                    double t = (double) (i + j) / SAMPLE_RATE;
-                    double freq = startFreq + (endFreq - startFreq) * (t / duration);
-                    buffer[j] = (short) (Math.sin(2 * Math.PI * freq * t) * Short.MAX_VALUE * 0.8);
-                }
-                if (mAudioTrack != null) {
-                    mAudioTrack.write(buffer, 0, Math.min(buffer.length, (int) (totalSamples - i)));
-                }
-            }
-            mHandler.post(this::stop);
-        }).start();
     }
 
-    private void stop() {
-        mPlaying = false;
-        if (mAudioTrack != null) {
-            mAudioTrack.stop();
-            mAudioTrack.release();
-            mAudioTrack = null;
-        }
-        mButton.setText(R.string.clear_speaker_title);
-        mStatus.setText("Done");
-    }
-
-    @Override
-    protected void onDestroy() {
-        stop();
-        super.onDestroy();
-    }
 }
