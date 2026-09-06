675da9d4def86b4c0de9797c33d8b513e68be263
Badmaneers <dukebraham24@gmail.com>
2026-09-06 10:28:07 +0530

    even: ship libion.vendor to satisfy blob dependency

--- device.mk
+++ device.mk
@@ -109,6 +109,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
     libvulkan \
     libdrm.vendor \
+    libion.vendor \
     libpower.vendor 
 
 PRODUCT_PACKAGES += \
